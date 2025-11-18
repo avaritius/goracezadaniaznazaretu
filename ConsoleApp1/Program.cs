05ce39f6e6501289ba952dbaccd6e2e419fbb900
4225203 <4225203@TE-GDA-D-D28J17>
2025-11-18 11:29:38 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 10, 15, 20, 25 };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine($" suma tonazu mamy nerda:{sum}");
+        }
+    }
+}
